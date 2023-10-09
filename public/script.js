f151472aa2c17770b964a147b794d9341881d2f0
Raymond <akdfhr2@gmail.com>
2023-10-09 23:23:32 +0900

    fix: chagne static file url

--- public/script.js
+++ public/script.js
@@ -1,99 +1,101 @@
 //const socket = io('http://localhost:3000')
-const socket = io('https://suhyun-ms-project2.onrender.com')
-const messageContainer = document.getElementById('message-container')
-const roomContainer = document.getElementById('room-container')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
-const video = document.getElementById('video')
-let currentEmotion = ""; 
+const socket = io("https://suhyun-ms-project2.onrender.com");
+const messageContainer = document.getElementById("message-container");
+const roomContainer = document.getElementById("room-container");
+const messageForm = document.getElementById("send-container");
+const messageInput = document.getElementById("message-input");
+const video = document.getElementById("video");
+let currentEmotion = "";
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+]).then(startVideo);
 
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+    (stream) => (video.srcObject = stream),
+    (err) => console.error(err)
+  );
 }
 
-video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
   //document.body.append(canvas)
-  document.getElementById("pp").appendChild(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
+  document.getElementById("pp").appendChild(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    console.log(detections[0]["expressions"])
-    console.log(`emotion :  ${selectEmotion(detections[0]["expressions"])}`)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 50)
-})
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    console.log(detections[0]["expressions"]);
+    console.log(`emotion :  ${selectEmotion(detections[0]["expressions"])}`);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+  }, 50);
+});
 
-function selectEmotion(_array){
-  if (_array.length == 0){
+function selectEmotion(_array) {
+  if (_array.length == 0) {
     return "감정 포착 실패!";
   }
   let emotion = "";
   let target = 0;
   for (const [key, value] of Object.entries(_array)) {
-    if(value>target){
+    if (value > target) {
       target = value;
       emotion = key;
     }
   }
-  return emotion
+  return emotion;
 }
 
 if (messageForm != null) {
-  const name = prompt('What is your name?')
-  appendMessage('You joined')
-  socket.emit('new-user', roomName, name)
+  const name = prompt("What is your name?");
+  appendMessage("You joined");
+  socket.emit("new-user", roomName, name);
 
-  messageForm.addEventListener('submit', e => {
-    e.preventDefault()
-    const message = messageInput.value
-    appendMessage(`You: ${message}`)
-    socket.emit('send-chat-message', roomName, message)
-    messageInput.value = ''
-  })
+  messageForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    appendMessage(`You: ${message}`);
+    socket.emit("send-chat-message", roomName, message);
+    messageInput.value = "";
+  });
 }
 
-socket.on('room-created', room => {
-  const roomElement = document.createElement('div')
-  roomElement.innerText = room
-  const roomLink = document.createElement('a')
-  roomLink.href = `/${room}`
-  roomLink.innerText = 'join'
-  roomContainer.append(roomElement)
-  roomContainer.append(roomLink)
-})
+socket.on("room-created", (room) => {
+  const roomElement = document.createElement("div");
+  roomElement.innerText = room;
+  const roomLink = document.createElement("a");
+  roomLink.href = `/${room}`;
+  roomLink.innerText = "join";
+  roomContainer.append(roomElement);
+  roomContainer.append(roomLink);
+});
 
-socket.on('chat-message', data => {
-  appendMessage(`${data.name}: ${data.message}`)
-})
+socket.on("chat-message", (data) => {
+  appendMessage(`${data.name}: ${data.message}`);
+});
 
-socket.on('user-connected', name => {
-  appendMessage(`${name} connected`)
-})
+socket.on("user-connected", (name) => {
+  appendMessage(`${name} connected`);
+});
 
-socket.on('user-disconnected', name => {
-  appendMessage(`${name} disconnected`)
-})
+socket.on("user-disconnected", (name) => {
+  appendMessage(`${name} disconnected`);
+});
 
 function appendMessage(message) {
-  const messageElement = document.createElement('div')
-  messageElement.innerText = message
-  messageContainer.append(messageElement)
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageContainer.append(messageElement);
 }
-
